a3f2c62231ef8192dcefbad985824f4d6727c14e
neelsaspara29 <106027223+neelsaspara29@users.noreply.github.com>
2022-11-25 10:49:44 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 import "react-toastify/dist/ReactToastify.css";
-import './Assets/css/fonts.css'
-import './Assets/css/swiper-bundle.min.css'
-import './Assets/css/style.css'
+import "./Assets/css/fonts.css";
+import "./Assets/css/swiper-bundle.min.css";
+import "./Assets/css/style.css";
 // import "swiper/swiper-bundle.min.css";
 // import "swiper/swiper.min.css";
-// import "./Style/tailindex.css" 
-import { Provider } from 'react-redux';
-import store, { persistor } from './Redux/store';
+// import "./Style/tailindex.css"
+import { Provider } from "react-redux";
+import store, { persistor } from "./Redux/store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
   //   <BrowserRouter>
-  //   <Provider store={store} persistor={persistor}>
-  //     <App />
-  //   </Provider>
+  //     <Provider store={store} persistor={persistor}>
+  //       <App />
+  //     </Provider>
   //   </BrowserRouter>
   // </React.StrictMode>
   <h1>neel</h1>
 );
-
